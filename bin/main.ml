ee91dda629b9ca9f7f7431d3929b7a331e8ab538
Térence Clastres <terence.clastres@univ-orleans.fr>
2025-06-18 13:38:06 +0200

    add program automaton printing

--- bin/main.ml
+++ bin/main.ml
@@ -9,17 +9,20 @@ let main (type triple_data) (type fol_data)
       with type triple_data = Middle.triple_data
        and type fol_data = Middle.fol_data) =
   let module Front = HardyFrontEnd in
-
+  let module P2Ba = HardyMiddleEnd.Buchi.Pg2ba.M in
+  let module P2BaDot = HardyMiddleEnd.Buchi.BuchiSig.Dot (P2Ba) in
   let module Cli = Cli () in
   let info = Cli.get_info in
   Parser.parse_file info.file |> Front.Typing.type_pgrm |> fun p ->
+  if not Sys.(file_exists info.outdir) then Sys.mkdir info.outdir 0o755;
+  Out_channel.with_open_text Filename.(concat info.outdir info.file ^ ".dot")
+  (fun o -> P2Ba.create p.prog_nodes |> P2BaDot.output_graph o);
   (if info.eval then
      Front.Interpreter.(
        eval_pgrm p (module Front.Interpreter.ConsoleBridge)));
   if info.verify then (
     let translate_spec = HardyMiddleEnd.Sig.translate_spec (module Middle) in
     let module Back = HardyBackEnd.Sig.F (Back) in
-    if not Sys.(file_exists info.outdir) then Sys.mkdir info.outdir 0o755;
     let output_file = Filename.(concat info.outdir @@ basename info.file) in
     translate_spec info p |> Back.translate_program p
     |> Back.write_program output_file)
---
+++ lib/middle_end/buchi/pg2ba.ml
@@ -0,0 +1,61 @@
+open HardyFrontEnd.Syntax
+open Program
+open HardyFrontEnd.Printer
+
+module Vertex : Graph.Sig.COMPARABLE with type t = string = struct
+  (* states are just labels *)
+  type t = string
+
+  let compare = String.compare
+  let hash = String.hash
+  let equal = String.equal
+end
+
+(* output of ltl2ba with formula for each arc *)
+module Arc : Graph.Sig.ORDERED_TYPE_DFT with type t = unit expr option = struct
+  type t = unit expr option
+
+  let compare = Stdlib.compare
+  let default = None
+end
+
+module M :
+  BuchiSig.S
+    with type E.label = unit expr option
+     and type init_val = (Shared.ty fol_t, unit) node list = struct
+  include Graph.Imperative.Digraph.ConcreteLabeled (Vertex) (Arc)
+
+  type init_val = (Shared.ty fol_t, unit) node list
+  type vdata = unit
+
+  let acceptant _ = false
+  let is_start_node (v : V.t) = v = "START"
+
+  let create (p : _ node list) : t =
+    let g = create ~size:(List.length p) () in
+    List.iter
+      (fun node ->
+        List.iter
+          (fun (guard, _, next) ->
+            let succ = Option.value next ~default:node.node_id in
+            let e = E.create (V.create node.node_id) guard (V.create succ) in
+            add_edge_e g e)
+          node.node_transitions)
+      p;
+    g
+
+  let string_of_vertex = Fun.id
+  let id_of_vertex = Fun.id
+
+  let string_of_edge (e : _ expr option) =
+    Format.(
+      asprintf "%a"
+        (pp_print_option (pp_exp (fun fmt (v, _) -> fprintf fmt "%s" v)))
+        e)
+
+  let get_vdata _ = ()
+
+  let get_edge_type (e : E.label) =
+    let open BuchiSig in
+    match e with Some _ -> Unknown | None -> Universal
+end
